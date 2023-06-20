4d5c532a908cf256bfe3a51032773774b808618b
jeevashanmugam <jeevasj2000@gmail.com>
2023-06-20 20:12:03 +0530

    JS-for-in-foreach-array-iteration

---
+++ main.js
@@ -0,0 +1,31 @@
+
+let arr = ["shanmugam", "jaya", "shalini", "jeeva", 18, 24, 02, 03]
+
+let person = {
+    name: "jeeva",
+    age: 22,
+    gender: "male",
+}
+
+arr.forEach(function (value, index, array) {
+
+    document.write(`${value} `);
+
+});
+
+
+//forEach does not return the object
+
+/*
+person.forEach(function (value, index, array) {
+
+    document.write(`${value} `);
+
+});
+
+*/
+
+
+
+
+
